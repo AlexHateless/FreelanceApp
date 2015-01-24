39ca6c73a1a192a4387b985ced5de835561e08f9
Kostiantyn <kon_sol@elance-odesk.com>
2015-01-25 00:00:56 +0200

    Task class updated

--- src/main/java/com/freelance/dao/entity/Task.java
+++ src/main/java/com/freelance/dao/entity/Task.java
@@ -1,8 +1,28 @@
 package com.freelance.dao.entity;
 
+import java.util.Date;
+
 /**
  * Created by user on 24.01.15.
  */
 public class Task {
+    private long id;
+    private String description;
+    private Date deadline;
+
+    public Task() {
+        deadline = new Date();
+    }
+
+    public Task(String description, Date deadline) {
+        this.description = description;
+        this.deadline = deadline;
+    }
+
+    public Task(long id, String description, Date deadline) {
+        this.id = id;
+        this.description = description;
+        this.deadline = deadline;
+    }
 
 }
