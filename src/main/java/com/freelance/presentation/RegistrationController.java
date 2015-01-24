0d52714f39a7f139d0f6b3f4908d8dfb8738d6f2
AlexFaster <4190200@gmail.com>
2015-01-25 00:23:13 +0200

    Add registration controller

---
+++ src/main/java/com/freelance/presentation/RegistrationController.java
@@ -0,0 +1,29 @@
+package com.freelance.presentation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by user on 25.01.15.
+ */
+@WebServlet
+public class RegistrationController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        PrintWriter pw = resp.getWriter();
+        pw.println("Login:" + login);
+        pw.println("Pass:" + password);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
