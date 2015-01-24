c015bfb2f66d469066a45d315b2ba3d989ae99b8
AlexFaster <4190200@gmail.com>
2015-01-25 00:12:12 +0200

    Comparable added

--- src/main/java/com/freelance/dao/entity/Task.java
+++ src/main/java/com/freelance/dao/entity/Task.java
@@ -5,12 +5,13 @@ import java.util.Date;
 /**
  * Created by user on 24.01.15.
  */
-public class Task {
+public class Task implements Comparable<Task>{
     private long id;
     private String description;
     private Date deadline;
 
     public Task() {
+        description="";
         deadline = new Date();
     }
 
@@ -25,4 +26,34 @@ public class Task {
         this.deadline = deadline;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        if (this.id > o.id) {
+            return 1;
+        } else if (this.id < o.id) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
